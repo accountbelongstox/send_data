3f705b6796f3513e1684ea65a42002394315767d
accountbelongstox <accountbelongstox@163.com>
2018-11-17 21:01:55 +0800

    autogit

--- 余生你好/send_data.js
+++ 余生你好/send_data.js
@@ -1,11 +1,11 @@
 const
-	file = "11.17GY-3500-s500.csv",
+	file = `3000多.csv`,
 	fs = require(`fs`),
 	path = require(`path`),
 	excel_xlsx = require('excel-xlsx'),
 	crypto = require('crypto'),
 	iconv = require(`iconv-lite`),
-	time_scope = [`2018-11-16 00:00:00`,`2018-11-16 23:20:00`],
+	time_scope = [`2018-11-11 00:00:00`,`2018-11-11 23:20:00`],
 	file_path = path.join(__dirname,file),
 	file_path_parse = path.parse(file_path),
 	new_file_path = path.join(__dirname,`${file_path_parse.name}`)
