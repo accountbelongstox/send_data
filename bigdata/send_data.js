e59991c325288217d5462abd03b8fd8bb7c39418
accountbelongstox <accountbelongstox@163.com>
2018-12-16 19:09:37 +0800

    autogit

--- bigdata/send_data.js
+++ bigdata/send_data.js
@@ -1,11 +1,11 @@
 let
-	file = `12.15.ss100.d.csv`,
+	file = `12.16.gy.200.csv`,
 	fs = require(`fs`),
 	path = require(`path`),
 	excel_xlsx = require('excel-xlsx'),
 	crypto = require('crypto'), 
 	iconv = require(`iconv-lite`),
-	time_scope = [`2018-12-15 11:00:00`,`-2018-12-15 15:00:59`],
+	time_scope = [`2018-12-15 22:28:00`,`2018-12-15 23:59:59`],
 	file_path = path.join(__dirname,file),
 	file_path_parse = path.parse(file_path),
 	file_new_name = file_path_parse.name,
