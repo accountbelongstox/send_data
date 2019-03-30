a90f413978ef3d4fc69cbfaeea7bc3e14660ba8b
accountbelongstox <accountbelongstox@163.com>
2019-03-30 13:14:57 +0800

    autogit

--- ooooo/send_data.js
+++ ooooo/send_data.js
@@ -1,11 +1,11 @@
 let
-	file = `3.28.gy.300.1.csv`,
+	file = `3.30.ss处理.csv`,
 	fs = require(`fs`),
 	path = require(`path`),
 	excel_xlsx = require('excel-xlsx'),
 	crypto = require('crypto'), 
 	iconv = require(`iconv-lite`),
-	time_scope = [`2019-3-27 0:00:00`,`2019-3-27 23:29:59`],
+	time_scope = [`2019-3-30 8:30:00`,`-2019-3-30 23:29:59`],
 	file_path = path.join(__dirname,file),
 	file_path_parse = path.parse(file_path),
 	file_new_name = file_path_parse.name,
