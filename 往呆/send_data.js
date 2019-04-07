2fe7f78dbba777ef023348bb63297a72ea74b6d1
accountbelongstox <accountbelongstox@163.com>
2019-04-07 20:16:53 +0800

    autogit

--- 往呆/send_data.js
+++ 往呆/send_data.js
@@ -1,11 +1,11 @@
 let
-	file = `4.5.ss.4000.1.csv`,
+	file = `4.7.ss处理.csv`,
 	fs = require(`fs`),
 	path = require(`path`),
 	excel_xlsx = require('excel-xlsx'),
 	crypto = require('crypto'), 
 	iconv = require(`iconv-lite`),
-	time_scope = [`2019-4-5 7:00:00`,`-2019-4-4 23:19:59`],
+	time_scope = [`2019-4-7 8:00:00`,`-2019-4-4 23:19:59`],
 	file_path = path.join(__dirname,file),
 	file_path_parse = path.parse(file_path),
 	file_new_name = file_path_parse.name,
